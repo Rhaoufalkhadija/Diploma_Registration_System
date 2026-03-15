4524d10805bacceca9c770acb67af4263dc46b32
Khadija RHAOUFAL <khadijarhaoufalyahoo.com>
2026-03-15 14:06:16 +0000

    Blockchain_backend_integration

--- diploma-blockchain-app/blockchain/scripts/deploy.js
+++ diploma-blockchain-app/blockchain/scripts/deploy.js
@@ -1,19 +1,15 @@
-const hre = require("hardhat");
+import hre from "hardhat";
 
 async function main() {
-
   console.log("Deploying contract...");
 
   const DiplomaRegistry = await hre.ethers.getContractFactory("DiplomaRegistry");
-
   const diplomaRegistry = await DiplomaRegistry.deploy();
 
   await diplomaRegistry.waitForDeployment();
 
   const address = await diplomaRegistry.getAddress();
-
   console.log("DiplomaRegistry deployed to:", address);
-
 }
 
 main().catch((error) => {
